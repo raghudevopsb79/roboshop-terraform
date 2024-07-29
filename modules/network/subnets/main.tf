00a474e65a329fcf442c6fb9d1db8c9cc8464e13
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-29 08:58:46 +0530

    RoboShop Terraform code

--- modules/network/subnets/main.tf
+++ modules/network/subnets/main.tf
@@ -33,33 +33,11 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_eip" "ngw" {
-  count  = var.name == "public" ? length(var.cidr) : 0
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "ngw" {
-  count         = var.name == "public" ? length(var.cidr) : 0
-  allocation_id = aws_eip.ngw.*.id[count.index]
-  subnet_id     = aws_subnet.subnet.*.id[count.index]
-
-  tags = {
-    Name = "${var.name}-${var.env}-${split("-", var.availability_zones[count.index])[2]}"
-  }
-}
-
 resource "aws_route" "igw-route" {
   count                  = var.name == "public" ? length(var.cidr) : 0
   route_table_id         = aws_route_table.route-table.*.id[count.index]
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.*.id[0]
-}
-
-resource "aws_route" "ngw-route" {
-  count                  = var.name != "public" ? length(var.cidr) : 0
-  route_table_id         = aws_route_table.route-table.*.id[count.index]
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.ngw.*.id[count.index]
 }
 
 
