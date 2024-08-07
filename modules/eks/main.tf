b09c3fb96b1ba436221238cc64322c314ce733f9
Raghu K <raghuk.vit@gmail.com>
2024-08-07 08:55:46 +0530

    RoboShop Terraform & Kubernetes Code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -32,12 +32,16 @@ resource "aws_eks_addon" "add_ons" {
   resolve_conflicts_on_update = each.value["resolve_conflicts_on_update"]
 }
 
+data "external" "oidc-thumbprint" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity[0].oidc[0].issuer ]
+}
+
 resource "aws_iam_openid_connect_provider" "main" {
   url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 
   client_id_list = [
     "sts.amazonaws.com",
   ]
-  thumbprint_list = []
+  thumbprint_list = [data.external.oidc-thumbprint.result.thumbprint]
 }
 
