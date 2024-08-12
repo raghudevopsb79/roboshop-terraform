0e7efa59bd8031f3e7eff06ed7b0e53bad0179a1
Raghu K <raghuk.vit@gmail.com>
2024-08-12 06:55:19 +0530

    RoboShop Terraform & Kubernetes Code

--- main.tf
+++ main.tf
@@ -1,45 +1,45 @@
-# module "network" {
-#   for_each = var.vpc
-#   source   = "./modules/network"
-#
-#   cidr               = each.value["cidr"]
-#   subnets            = each.value["subnets"]
-#   availability_zones = each.value["availability_zones"]
-#   peering_vpcs       = each.value["peering_vpcs"]
-#
-#   env = var.env
-# }
-#
-# module "db" {
-#   source   = "./modules/ec2"
-#   for_each = var.db_servers
-#
-#   env           = var.env
-#   bastion_nodes = var.bastion_nodes
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-#
-#   name          = each.key
-#   ports         = each.value["ports"]
-#   instance_type = each.value["instance_type"]
-#
-#   vpc_id     = module.network["main"].vpc_id
-#   subnet_ids = module.network["main"].subnets["db"].subnets
-# }
-#
-# module "eks" {
-#   source   = "./modules/eks"
-#   for_each = var.eks
-#
-#   env = var.env
-#
-#   eks_version = each.value["eks_version"]
-#   name        = each.key
-#   node_groups = each.value["node_groups"]
-#   add_ons     = each.value["add_ons"]
-#
-#   subnet_ids = module.network["main"].subnets[each.value["subnet_ref"]].subnets
-# }
+module "network" {
+  for_each = var.vpc
+  source   = "./modules/network"
+
+  cidr               = each.value["cidr"]
+  subnets            = each.value["subnets"]
+  availability_zones = each.value["availability_zones"]
+  peering_vpcs       = each.value["peering_vpcs"]
+
+  env = var.env
+}
+
+module "db" {
+  source   = "./modules/ec2"
+  for_each = var.db_servers
+
+  env           = var.env
+  bastion_nodes = var.bastion_nodes
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+
+  name          = each.key
+  ports         = each.value["ports"]
+  instance_type = each.value["instance_type"]
+
+  vpc_id     = module.network["main"].vpc_id
+  subnet_ids = module.network["main"].subnets["db"].subnets
+}
+
+module "eks" {
+  source   = "./modules/eks"
+  for_each = var.eks
+
+  env = var.env
+
+  eks_version = each.value["eks_version"]
+  name        = each.key
+  node_groups = each.value["node_groups"]
+  add_ons     = each.value["add_ons"]
+
+  subnet_ids = module.network["main"].subnets[each.value["subnet_ref"]].subnets
+}
 
 module "opensearch" {
   source   = "./modules/opensearch"
